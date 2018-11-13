9d9e14c9af06752dd16effc3151d293f472519e1
First Last <21mleingang@gmail.com>
2018-11-13 13:18:33 -0500

    completes exercises 1-4 and starts exercise 10

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -4,7 +4,8 @@
 
 function hello() {
 
-  // WRITE YOUR EXERCISE 1 CODE HERE
+  var div = document.getElementById('output1');
+  div.innerHTML = "Hello, AP Computer Science Principles!";
 
   ////////////////// DO NOT MODIFY
   check("hello"); // DO NOT MODIFY
@@ -21,10 +22,11 @@ function helloAgain() {
   let name; // DO NOT MODIFY
   //////////// DO NOT MODIFY
 
-  // Use the name variable declared above to store the user's response. You
-  // do not need to re-declare it, only assign it a value.
+    var div = document.getElementById("output2");
 
-  // WRITE YOUR EXERCISE 2 CODE HERE
+    name = prompt("What is your name?")
+    alert("Hello, " + name + "!");
+    div.innerHTML = "Hello, " + name + "!";
 
   ///////////////////////////// DO NOT MODIFY
   check("helloAgain", name); // DO NOT MODIFY
@@ -40,11 +42,10 @@ function celsius() {
   //////////////////////////////////////////////////////// DO NOT MODIFY
   let cels = ((Math.random() * 1001) - 100).toFixed(2); // DO NOT MODIFY
   //////////////////////////////////////////////////////// DO NOT MODIFY
+  var celsCalculation = cels * 1.8 + 32;
 
-  // The above code generates a random number between -100 and 1000
-  // (inclusive), and rounds this value to 2 decimal places.
-
-  // WRITE YOUR EXERCISE 3 CODE HERE
+  var div = document.getElementById('output3');
+  div.innerHTML = cels + " degrees Celsius equals " + celsCalculation.toFixed(2) + " degrees Fahrenheit.";
 
   ////////////////////////// DO NOT MODIFY
   check("celsius", cels); // DO NOT MODIFY
@@ -63,8 +64,11 @@ function fahrenheit() {
 
   // The above code generates a random number between -100 and 1000
   // (inclusive), and rounds this value to 2 decimal places.
+  var fahrCalculation = (fahr - 32) * 5/9
 
-  // WRITE YOUR EXERCISE 4 CODE HERE
+  var div = document.getElementById('output4');
+  div.innerHTML = fahr + " degrees Fahrenheit equals " + fahrCalculation.toFixed(2) + " degrees Celsius.";
+
 
   ///////////////////////////// DO NOT MODIFY
   check("fahrenheit", fahr); // DO NOT MODIFY
@@ -79,16 +83,10 @@ function inches() {
 
   /////////////////////////////////////////////////////// DO NOT MODIFY
   let input = prompt("Enter a non-negative integer."); // DO NOT MODIFY
+  let inches = input;                                  // DO NOT MODIFY
   /////////////////////////////////////////////////////// DO NOT MODIFY
 
-  // You are free to modify the value of inches, which you'll likely
-  // need to do. Please do not modify the value of input.
-
-  ////////////////////// DO NOT MODIFY
-  let inches = input; // DO NOT MODIFY
-  ////////////////////// DO NOT MODIFY
-
-  // WRITE YOUR EXERCISE 5 CODE HERE
+  var div = document.getElementById("output5")
 
   ////////////////////////// DO NOT MODIFY
   check("inches", input); // DO NOT MODIFY
@@ -184,7 +182,7 @@ function money() {
   let pennies = input; // DO NOT MODIFY
   /////////////////////// DO NOT MODIFY
 
-  // WRITE YOUR EXERCISE 9 CODE HERE
+
 
   ///////////////////////// DO NOT MODIFY
   check("money", input); // DO NOT MODIFY
@@ -208,7 +206,12 @@ function change() {
   let amount = input; // DO NOT MODIFY
   ////////////////////// DO NOT MODIFY
 
-  // WRITE YOUR EXERCISE 10 CODE HERE
+  var div = document.getElementById("output10")
+  div.innerHTML = quarters + dimes + nickels + pennies;
+
+  if(amount >= 0.25){
+    var quarters = amount / 0.25;
+  }
 
   ////////////////////////// DO NOT MODIFY
   check("change", input); // DO NOT MODIFY
